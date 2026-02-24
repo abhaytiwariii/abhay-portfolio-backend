774b839032679c95dee12d5442bfb68735f0f7a8
abhaytiwariii <at1384424@gmail.com>
2026-02-24 15:14:25 +0530

    fix: add a sepecific url for production and development

--- app/main.py
+++ app/main.py
@@ -13,7 +13,10 @@ app = FastAPI(title="Abhay Portfolio API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+allow_origins=[
+        "https://abhaytiwariii.vercel.app",
+        "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
